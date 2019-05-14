f84448d912b26baf06a028e13d5803ce414bb26f
TylerGlisson <tyler@tglisson.com>
2019-05-14 14:10:33 -0400

    smooth touch scrolling on mobile

--- js/main.js
+++ js/main.js
@@ -28,3 +28,11 @@ const navSlide = () => {
 }
 
 navSlide();
+
+document.onload( () => {
+  var deviceAgent = navigator.userAgent.toLowerCase();
+  var agentID = deviceAgent.match(/(iPad|iPhone|iPod)/i);
+  if (agentID) {       
+    document.querySelector('body').css('-webkit-overflow-scrolling', 'touch');
+  }
+});
