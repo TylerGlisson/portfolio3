f93a5014131aef8b10330e08e32ec4642cfc6c02
TylerGlisson <tyler@tglisson.com>
2019-05-14 14:21:03 -0400

    removed jQuery from scrolling

--- js/main.js
+++ js/main.js
@@ -29,10 +29,11 @@ const navSlide = () => {
 
 navSlide();
 
-document.onload( () => {
+window.addEventListener('DOMContentLoaded', (event) => {
   var deviceAgent = navigator.userAgent.toLowerCase();
   var agentID = deviceAgent.match(/(iPad|iPhone|iPod)/i);
   if (agentID) {       
-    document.querySelector('body').css('-webkit-overflow-scrolling', 'touch');
+    let scrolling = document.querySelector('body');
+    scrolling.style.cssText += "'-webkit-overflow-scrolling', 'touch'"; 
   }
 });
